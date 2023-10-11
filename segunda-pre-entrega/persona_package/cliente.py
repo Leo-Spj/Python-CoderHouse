17dd1a78f3cda9eed3b59ee02d08361dec97981a
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-10-10 22:16:31 -0500

    añadiendo metodos accesores

--- segunda-pre-entrega/persona_package/cliente.py
+++ segunda-pre-entrega/persona_package/cliente.py
@@ -19,6 +19,37 @@ class Cliente(persona.Persona):
         self.estado = estado
         self.fecha_alta = fecha_alta
 
+    def get_codigo_cliente(self):
+        return self.codigo_cliente
+    
+    def set_codigo_cliente(self, codigo_cliente):
+        self.codigo_cliente = codigo_cliente
+    
+    def get_tipo_cliente(self):
+        return self.tipo_cliente
+    
+    def set_tipo_cliente(self, tipo_cliente):
+        self.tipo_cliente = tipo_cliente
+
+    def get_estado(self):
+        return self.estado
+    
+    def set_estado(self, estado):
+        self.estado = estado
+
+    def get_fecha_alta(self):
+        return self.fecha_alta
+    
+    def set_fecha_alta(self, fecha_alta):
+        self.fecha_alta = fecha_alta
+
+    def get_fecha_baja(self):
+        return self.fecha_baja
+    
+    def set_fecha_baja(self, fecha_baja):
+        self.fecha_baja = fecha_baja
+    
+
     def __str__(self):
         return super().__str__() + ", codigo: " + str(self.codigo_cliente) + ", tipo: " + self.tipo_cliente + ", estado: " + self.estado + ", fecha alta: " + self.fecha_alta
 
